ea6f7fc308face207b2d0fe01aa487bedc01b573
quangphucc0903 <nguyenquangphuc0309@gmail.com>
2024-03-19 16:41:33 +0700

    be

---
+++ admin/config.php
@@ -0,0 +1,11 @@
+<?php
+/** The name of the database*/
+define( 'DB_NAME','nhom5' );
+/** MySQL database username */
+define( 'DB_USER', 'root' );
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+/** port number of DB */
+define( 'PORT', 3306);
